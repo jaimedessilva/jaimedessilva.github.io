386e716fb2c9b23798ca86dd5d3de21e20446164
jaime <jaime.jss>
2021-07-11 22:05:36 -0300

    first commit

---
+++ js/links.js
@@ -0,0 +1,49 @@
+let listLinksServ = []
+let listLinks = []
+let listLinksContatos = []
+
+const getLinks = () => {
+    
+    //Links Portifolio
+   
+    listLinks.push(new Url("https://app-sgveiculos.herokuapp.com/", "SG Veiculos (Java)", './images/sg_veiculos.png'))
+    listLinks.push(new Url("https://urna-eletronica.netlify.app/", "Urna eletronica (JS)", './images/urna_eletronica.png'))
+    listLinks.push(new Url("http://open-movie.netlify.app/", "OpenMovie (JS)", './images/ssj_filmes.png'))
+    listLinks.push(new Url("/pokemon/index.html", "Pokemon Api", './images/poke_api.jpg'))
+    listLinks.push(new Url("https://app-login-security.herokuapp.com/", "Login Spring Security", "./images/springSecurity.jpg"))
+    listLinks.push(new Url("https://getapi/", "GET APIS",))
+    listLinks.push(new Url("https://shero/api", "S-Hero",))
+
+    return listLinks
+}
+
+const getLinkServicos = () => {
+    //links dos Serviços    
+    let list = []
+
+    list.push(new Url("https://www.netlify.com/", "Netlify"))
+    list.push(new Url("https://www.heroku.com/", "Heroku Cloud"))
+    list.push(new Url("https://www.themoviedb.org/", "TMDB Api"))
+    list.push(new Url("https://superheroapi.com/", "Super Hero Api"))
+    list.push(new Url("https://developer.marvel.com/", "Marvel API"))
+    list.push(new Url("https://github.com/", "GitHub"))
+    
+    listLinksServ = list
+
+    return listLinksServ
+}
+//List Links Rodapé
+const getLinksContatos = () => {
+ 
+    listLinksContatos.push({ url: "http://api.whatsapp.com/send?1=pt_BR&phone=5561995411760", title: "Whatsapp", class: "fa fa-whatsapp"})
+    listLinksContatos.push({ url: "https://github.com/jaimedessilva", title: "GitHub", class: "fa fa-github"})
+    listLinksContatos.push({ url: "https://www.linkedin.com/in/jaimedessilva/", title: "Linkedin", class: "fa fa-linkedin"})
+    listLinksContatos.push({ url: "https://www.youtube.com/channel/UCcPgsJPn7K4LxdRnOWunfPQ", title: "Youtube", class: "fa fa-youtube"})
+    listLinksContatos.push({ url: "#google", title: "Google", class: "fa fa-google"})
+    listLinksContatos.push({ url: "#reddit", title: "Reddit", class: "fa fa-reddit"})
+    listLinksContatos.push({ url: "#rss", title: "RSS", class: "fa fa-rss"})
+    listLinksContatos.push({ url: "#android", title: "Android", class: "fa fa-android"})
+    
+    return listLinksContatos
+
+}
---
+++ js/site.js
@@ -0,0 +1,88 @@
+const li = document.querySelector('.gallery')
+const lk = document.querySelector('.lk')
+const cards = document.querySelector('.cards')
+const topnav = document.querySelector('.topnav')
+const about = document.querySelector('.about')
+const linksContato = document.querySelector('.links-contato')
+const avatar = document.querySelector('.avatar')
+const menu = document.querySelector('.menu-ul')
+const list = []
+
+let html = ''
+
+
+//Script Menu
+/* const myFunction = () => {
+   let x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+          x.className += " responsive";
+      } else {
+          x.className = "topnav";
+      }
+} */
+ //Menu
+        
+const responsiveMenu = () => {
+  if(menu.style.display == 'block'){
+      menu.style.display = 'none'
+  }
+  else {
+      menu.style.display = 'block'
+   }
+}
+
+//Links Portifolio
+for (let link of getLinks()) {
+
+   html += `<div class="card">`
+   html += `<div class="card-img">`
+   html += `<div class="card-text">`
+   html += `${link.img}`
+   html += `</div>`
+   html += `<h3>${link.title}</h3>`
+   html += `<p><a href="${link.url}">${link.url}</a></p>`
+   html += `</div></div>`
+}
+cards.innerHTML = html
+
+//Links de Tecnologias
+let link = ''
+for (let href of getLinkServicos()) {
+   link += '<li>'
+   link += href.link
+   link += '</li>'
+}
+lk.innerHTML = link;
+
+//Scroll Menu  
+(window.onscroll = () => {
+   if (window.pageYOffset > 100) {
+      topnav.style.background = "#000"
+      topnav.style.opacity = "0.5"
+   }
+   else {
+      topnav.style.background = "#000"
+      topnav.style.opacity = "1"
+   }
+})();
+
+//About
+(sobre = () => {
+   avatar.innerHTML = `<img src="./images/foto-jaime.jpg" alt="foto">`
+   about.innerHTML = `<h4 class="text-left">Sobre</h4><i>Sou Jaime</i>, 
+formado em Analise e Desenvolvimento de Sistemas e apaixonado por tecnologia. 
+Desenvolvi esse portfólio com o intuito de demonstrar alguns dos projetos que eu já desenvolvi e os que eu vier a
+desenvolver posteriormente vou colocando aqui. Tenho experiência nas tecnologias e Linguagens: Java, Spring,
+Spring-Boot, JEE, JPA, Hibernate, Bancos de dados Postgres, MySQL/Maria-DB e Oracle. Tenho também conhecimentos em
+desenvolvimento Web com Tecnologias modernas tais como HTML5, CSS, Javascript ES6, Angular, AngularJS.`
+
+})();
+
+//Links de contato
+(contatos = () => {
+   html = ''                 
+   for(link of getLinksContatos()){
+      html += `<a href="${link.url}" class="${link.class}"></a>`
+   }
+   linksContato.innerHTML = html   
+})()
