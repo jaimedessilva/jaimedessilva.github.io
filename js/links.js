c00923987a45c06314b589428303ff732a2e08ec
jaime <jaime.jss>
2021-07-12 19:22:51 -0300

    ajustes background

--- js/links.js
+++ js/links.js
@@ -26,23 +26,23 @@ const getLinks = () => {
 const getLinkServicos = () => {
     //links dos Serviços    
     let list = []
-
+    
+    list.push(new Url("https://github.com/", "GitHub"))
     list.push(new Url("https://www.netlify.com/", "Netlify"))
     list.push(new Url("https://www.heroku.com/", "Heroku Cloud"))
     list.push(new Url("https://www.themoviedb.org/", "TMDB Api"))
     list.push(new Url("https://superheroapi.com/", "Super Hero Api"))
     list.push(new Url("https://developer.marvel.com/", "Marvel API"))
-    list.push(new Url("https://github.com/", "GitHub"))
-    
+      
     listLinksServ = list
 
     return listLinksServ
 }
 //List Links Rodapé
 const getLinksContatos = () => {
- 
-    listLinksContatos.push({ url: "http://api.whatsapp.com/send?1=pt_BR&phone=5561995411760", title: "Whatsapp", class: "fa fa-whatsapp"})
+    
     listLinksContatos.push({ url: "https://github.com/jaimedessilva", title: "GitHub", class: "fa fa-github"})
+    listLinksContatos.push({ url: "http://api.whatsapp.com/send?1=pt_BR&phone=5561995411760", title: "Whatsapp", class: "fa fa-whatsapp"})
     listLinksContatos.push({ url: "https://www.linkedin.com/in/jaimedessilva/", title: "Linkedin", class: "fa fa-linkedin"})
     listLinksContatos.push({ url: "https://www.youtube.com/channel/UCcPgsJPn7K4LxdRnOWunfPQ", title: "Youtube", class: "fa fa-youtube"})
     listLinksContatos.push({ url: "#google", title: "Google", class: "fa fa-google"})
